3b3262e20103f97d47143d858afd3a1914852098
CG3599 <96074416+CG3599@users.noreply.github.com>
2026-09-23 20:57:44 +0800

    Use CWA documented URL authorization for server-side request

--- api/weather.js
+++ api/weather.js
@@ -9,14 +9,11 @@ export default async function handler(req, res) {
   }
 
   const url = new URL("https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-D0047-093");
+  url.searchParams.set("Authorization", key);
   url.searchParams.set("format", "JSON");
 
   try {
-    const response = await fetch(url.toString(), {
-      headers: {
-        Authorization: key
-      }
-    });
+    const response = await fetch(url.toString());
 
     const text = await response.text();
     res.status(response.status);
